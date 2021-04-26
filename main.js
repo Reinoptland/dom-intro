5c06de5cd75e6791ffbdc920c370dc62efb5d9f1
Reinoptland <reinoptland@gmail.com>
2021-04-26 18:45:54 +0200

    Change the dom in 3 ways

--- main.js
+++ main.js
@@ -3,4 +3,21 @@
 
 // element selecteren
 const header = document.getElementById("bigTitle");
-console.log(header);
+// elements are like objects
+// properties:
+console.log("INNERTEXT:", header.textContent);
+console.log("ID", header.id);
+console.log("Color", header.className);
+
+// giving properties another value
+header.textContent = "YOU GOT HACKED";
+
+// calling methods on elements
+header.remove();
+
+// creating new elements from scratch
+const newText = document.createElement("p");
+newText.textContent = "I AM A NEW PARAGRAPH!";
+// and appending them
+document.body.appendChild(newText);
+console.log(newText);
